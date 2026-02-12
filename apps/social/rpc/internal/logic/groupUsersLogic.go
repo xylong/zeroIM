1c7c82e9711d2208ca446a750027a793255a425b
xyl <416319808@qq.com>
2026-02-12 22:46:50 +0800

    群成员列表

--- apps/social/rpc/internal/logic/groupUsersLogic.go
+++ apps/social/rpc/internal/logic/groupUsersLogic.go
@@ -2,6 +2,12 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"github.com/jinzhu/copier"
+	errors2 "github.com/pkg/errors"
+	"gorm.io/gorm"
+	"zeroIM/apps/social/models"
+	"zeroIM/pkg/xerr"
 
 	"zeroIM/apps/social/rpc/internal/svc"
 	"zeroIM/apps/social/rpc/social"
@@ -24,7 +30,24 @@ func NewGroupUsersLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GroupU
 }
 
 func (l *GroupUsersLogic) GroupUsers(in *social.GroupUsersReq) (*social.GroupUsersResp, error) {
-	// todo: add your logic here and delete this line
+	members, err := l.svcCtx.Dao.GroupMember.WithContext(l.ctx).
+		Where(l.svcCtx.Dao.GroupMember.GroupID.Eq(in.GroupId)).
+		Find()
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, errors2.Wrapf(xerr.NewDBErr(), "group member by groupid err %v groupid %v", err, in.GroupId)
+	}
 
-	return &social.GroupUsersResp{}, nil
+	return &social.GroupUsersResp{
+		List: l.toList(members),
+	}, nil
+}
+
+func (l *GroupUsersLogic) toList(members []*models.GroupMember) []*social.GroupMembers {
+	if len(members) == 0 {
+		return nil
+	}
+
+	var list []*social.GroupMembers
+	_ = copier.Copy(&list, members)
+	return list
 }
